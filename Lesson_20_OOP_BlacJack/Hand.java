aebde8da022634c87c57f2c4b23fd772f5cf6a7a
RudnitskyAlexander <108361831+RudnitskyAlexander@users.noreply.github.com>
2022-09-29 21:28:31 +0300

    Add files via upload

---
+++ Lesson_20_OOP_BlacJack/Hand.java
@@ -0,0 +1,38 @@
+package Lesson_20_OOP_BlacJack;
+
+import java.util.ArrayList;
+
+public abstract class Hand {
+
+    protected ArrayList<Card> hand;
+//?  String name
+
+    Hand() {
+        hand = new ArrayList<>();
+    }
+
+    public void takeCard(Deck deck) {
+        hand.add(deck.sendCard());
+    }
+
+    public String toString() {
+        String res = "";
+        for (int i = 0; i < hand.size(); i++) {
+            res += hand.get(i).toString();
+        }
+        return res;
+    }
+
+    public int calcScore() {
+        // TODO сделать что бы туз давал 1 или 11 очков
+        int score = 0;
+        for (Card card : hand) {
+            score += card.getRank();
+        }
+        return score;
+    }
+
+    public abstract void play();
+
+
+}
---
+++ Lesson_20_OOP_BlacJack/Lesson_20_OOP_BlackJack.java
@@ -0,0 +1,11 @@
+package Lesson_20_OOP_BlacJack;
+
+import sun.security.krb5.internal.crypto.Des;
+
+public class Lesson_20_OOP_BlackJack {
+    public static void main(String[] args) {
+
+        Deck deck = new Deck();
+        System.out.println(deck);
+    }
+}
