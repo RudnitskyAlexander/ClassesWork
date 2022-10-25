1bf238b7f072c64bd1c7491c5e80d31cde8be229
RudnitskyAlexander <108361831+RudnitskyAlexander@users.noreply.github.com>
2022-10-25 21:30:24 +0300

    Add files via upload

---
+++ Lesson_27_Tree/BinaryTree.java
@@ -0,0 +1,15 @@
+package Lesson_27_Tree; // binary search tree
+
+public interface BinaryTree<T> {
+    boolean isEmpty();
+
+    void add(T data);
+
+    Node<T> findMin(Node<T> root);
+
+    T findMax();
+
+    void delete(T data);
+
+    boolean contains(T data);
+}
