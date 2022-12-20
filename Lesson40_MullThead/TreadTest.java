f59e7420c3498deb45c95ffd719784e11de9461c
RudnitskyAlexander <108361831+RudnitskyAlexander@users.noreply.github.com>
2022-12-20 21:21:15 +0300

    Add files via upload

---
+++ Lesson40_MullThead/TreadTest.java
@@ -0,0 +1,105 @@
+package Lesson40_MullThead;
+
+import jdk.nashorn.api.tree.ThrowTree;
+
+import javax.swing.plaf.TableHeaderUI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TreadTest {
+    public static int counter = 0;
+    public static Object locker = new Object();
+
+    public static void main(String[] args) {
+//        int limit = 500_000;
+//        IncThread t6 = new IncThread(limit);
+//        DecThread t7 = new DecThread(limit);
+//        t6.start();
+//        t7.start();
+//        try {
+//            t6.join();
+//            t7.join();
+//        } catch (InterruptedException ex) {
+//            ex.printStackTrace();
+//            Logger.getLogger(TreadTest.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        System.out.println("counter= " + counter);
+
+        MyResource res = new MyResource();
+        Thread t1 = new MyThread(res);
+        Thread t2 = new MyThread(res);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+
+        }
+        System.out.println("res+ " + res.getCounter());
+    }
+}
+
+class IncThread extends Thread {
+    int limit;
+
+    public IncThread(int limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < limit; i++) {
+            synchronized (TreadTest.locker) {
+                TreadTest.counter++;
+            }
+        }
+    }
+}
+
+class DecThread extends Thread {
+    int limit;
+
+    public DecThread(int limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < limit; i++) {
+            synchronized (TreadTest.locker) {
+                TreadTest.counter--;
+            }
+        }
+    }
+}
+
+class MyResource {
+    long counter = 0;
+
+    public synchronized void add(long value) {
+        this.counter += value;
+    }
+
+    long getCounter() {
+        return this.counter;
+    }
+}
+
+class MyThread extends Thread {
+    protected MyResource obj_counter = null;
+
+    public MyThread(MyResource counter) {
+        this.obj_counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 10000; i++) {
+            obj_counter.add(i);
+        }
+    }
+}
+
+
